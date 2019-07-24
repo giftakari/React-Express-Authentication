5cd1433df222db30ae265472a3e24eb2b51bf8c9
Godsgift ThankGod Akari <giftakari@yahoo.com>
2019-07-24 12:14:49 +1000

    Sign Up Form

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+//GET /Register
+
+router.get("/register", (req, res, next) => {
+  return res.render("register", { title: " Signup" });
+});
+
+// Post/ Register
+router.post("/register", (req, res, next) => {
+  return res.send("user created");
+});
+
 // GET /
-router.get('/', function(req, res, next) {
-  return res.render('index', { title: 'Home' });
+router.get("/", function(req, res, next) {
+  return res.render("index", { title: "Home" });
 });
 
 // GET /about
-router.get('/about', function(req, res, next) {
-  return res.render('about', { title: 'About' });
+router.get("/about", function(req, res, next) {
+  return res.render("about", { title: "About" });
 });
 
 // GET /contact
-router.get('/contact', function(req, res, next) {
-  return res.render('contact', { title: 'Contact' });
+router.get("/contact", function(req, res, next) {
+  return res.render("contact", { title: "Contact" });
 });
 
 module.exports = router;
